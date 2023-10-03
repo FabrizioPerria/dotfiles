9da1ffb0b1ebc82ceaa4d4f0ef1cef2e3a7addf4
Fabrizio Perria <fabrizio.perria@gmail.com>
2023-10-03 10:32:28 +0200

    omz plugins

--- shell/zsh.zsh
+++ shell/zsh.zsh
@@ -1,5 +1,6 @@
 export ZSH="$HOME/.oh-my-zsh"
 
 ZSH_THEME="powerlevel10k/powerlevel10k"
-
+ZSH_TMUX_UNICODE=1
+plugins=( git zsh-syntax-highlighting zsh-autosuggestions fzf tmux web-search thefuck sudo ripgrep )
 source $ZSH/oh-my-zsh.sh
