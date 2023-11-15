8e009cd89939d29721b567504c37ca1e05182c07
Fabrizio Perria <fabrizio.perria@gmail.com>
2023-11-15 19:50:22 +0100

    Add further instructions

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@ set -e
 if [[ -d ${HOME}/.config ]]; then
     rm -rf ${HOME}/.config.backup
     mv ${HOME}/.config ${HOME}/.config.backup
-    mkdir ${HOME}/.config
 fi
 
 if [[ -f ${HOME}/.zshrc ]]; then 
@@ -60,11 +59,15 @@ echo 'source ${HOME}/.config/fzf/completion.sh'>> ${HOME}/.zshrc
 echo 'source ${HOME}/.config/fzf/keybindings.sh'>> ${HOME}/.zshrc
 echo 'source ${HOME}/.config/shell/colors.zsh'>> ${HOME}/.zshrc
 
-mkdir -p ${HOME}/.config/fzf
+mkdir ${HOME}/.config
+
+mkdir ${HOME}/.config/fzf
 cp -r fzf/* ${HOME}/.config/fzf
-mkdir -p ${HOME}/.config/nvim
-cp -r nvim/* ${HOME}/.config
-mkdir -p ${HOME}/.config/shell
-cp -r shell/* ${HOME}/.config
-mkdir -p ${HOME}/.config/tmux
-cp -r tmux/* ${HOME}/.config
+mkdir ${HOME}/.config/nvim
+cp -r nvim/* ${HOME}/.config/nvim
+mkdir ${HOME}/.config/shell
+cp -r shell/* ${HOME}/.config/shell
+mkdir ${HOME}/.config/tmux
+cp -r tmux/* ${HOME}/.config/tmux
+
+echo "Now you can reopen your terminal and run nvim +PackerSync to install plugins"
