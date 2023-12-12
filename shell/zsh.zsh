a6a34ae0def3b49c931a980302daef98b6de2426
Fabrizio Perria <fabrizio.perria@gmail.com>
2023-12-12 18:49:02 +0100

    Update zsh.zsh

--- shell/zsh.zsh
+++ shell/zsh.zsh
@@ -2,9 +2,11 @@ export ZSH="$HOME/.oh-my-zsh"
 
 ZSH_THEME="powerlevel10k/powerlevel10k"
 ZSH_TMUX_UNICODE=1
-plugins=( git zsh-syntax-highlighting zsh-autosuggestions fzf web-search thefuck sudo ripgrep zsh-exa forgit)
-bindkey -M emacs '^\e' sudo-command-line
-bindkey -M vicmd '^\e' sudo-command-line
-bindkey -M viins '^\e' sudo-command-line
 
+plugins=( git zsh-syntax-highlighting zsh-autosuggestions fzf web-search thefuck sudo ripgrep zsh-exa forgit)
+if [ "$(uname)" == "Darwin" ]; then
+  bindkey -M emacs '^\e' sudo-command-line
+  bindkey -M vicmd '^\e' sudo-command-line
+  bindkey -M viins '^\e' sudo-command-line
+fi
 source $ZSH/oh-my-zsh.sh
