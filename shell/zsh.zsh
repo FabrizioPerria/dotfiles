88eaa1c6997240ceb47406b2cdbcd24d9551709f
Fabrizio Perria <fabrizio.perria@gmail.com>
2023-12-13 08:37:25 +0100

    Update zsh.zsh

--- shell/zsh.zsh
+++ shell/zsh.zsh
@@ -4,7 +4,7 @@ ZSH_THEME="powerlevel10k/powerlevel10k"
 ZSH_TMUX_UNICODE=1
 
 plugins=( git zsh-syntax-highlighting zsh-autosuggestions fzf web-search thefuck sudo ripgrep zsh-exa forgit)
-if [ "$(uname)" == "Darwin" ]; then
+if [ "$(uname)" = "Darwin" ]; then
   bindkey -M emacs '^\e' sudo-command-line
   bindkey -M vicmd '^\e' sudo-command-line
   bindkey -M viins '^\e' sudo-command-line
