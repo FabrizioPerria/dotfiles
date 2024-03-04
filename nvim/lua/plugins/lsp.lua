7d5f7dcab79daea443fd432d72503190759c142f
Fabrizio Perria <fabrizio.perria@gmail.com>
2024-03-04 22:48:16 +0100

    Use fork for nvim-cmp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,5 +1,23 @@
 return {
   {
+    "llllvvuu/nvim-cmp",
+    branch = "feat/above",
+    opts = function()
+      require("cmp").setup({
+        view = {
+          entries = {
+            name = "custom",
+            selection_order = "top_down",
+            vertical_positioning = "above",
+          },
+          docs = {
+            auto_open = true,
+          },
+        },
+      })
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
     opts = {
       setup = {
