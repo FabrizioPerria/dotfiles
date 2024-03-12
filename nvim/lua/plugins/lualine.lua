645b32be2f361cada55e6943edf4426434c7315c
Fabrizio Perria <fabrizio.perria@gmail.com>
2024-03-12 22:17:27 +0100

    Setup lualine

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,20 @@
+function GetCurrentWorkingDirectory()
+    return vim.fn.getcwd()
+end
+
+return {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+        sections = {
+            -- lualine_c = { {'filename', path=1} }
+            lualine_c = {
+                { "GetCurrentWorkingDirectory()", icons_enabled = true, icon = "" },
+                { "filename", path = 1, icons_enabled = true, icon = "" },
+            },
+            lualine_x = { "encoding", "fileformat", "filetype" },
+            lualine_y = { "progress", "location" },
+            lualine_z = { "diagnostics" },
+        },
+    },
+}
