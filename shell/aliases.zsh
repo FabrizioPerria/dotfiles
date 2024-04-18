fde63d98dbd8ebe1e7810ce8374627c99f421f2c
Fabrizio Perria <fabrizio.perria@gmail.com>
2024-04-18 18:35:17 +0200

    Harpoon

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -4,3 +4,4 @@ alias vim=nvim
 alias tmux="tmux -u a || tmux -u "
 alias mon="btm"
 alias rf='f(){ rg --hidden -l "$1" . | fzf | xargs nvim; }; f'
+alias gittree="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'"
