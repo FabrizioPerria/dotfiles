ccf11f7218a670c809745fcfba6a791a8eef1b9e
Fabrizio Perria <fabrizio.perria@gmail.com>
2024-07-08 21:26:41 +0200

    Disable nvim crutches

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -35,18 +35,32 @@ return {
             })
 
             lsp_zero.on_attach(function(client, bufnr)
+                local opts = { buffer = bufnr }
                 vim.keymap.set('n', '<leader>a', function() require("actions-preview").code_actions() end, opts)
                 vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
                 vim.keymap.set("n", "<leader>fo", '<cmd> lua vim.lsp.buf.format()<cr>', opts)
                 vim.keymap.set("n", "<leader>li", '<cmd>LspInfo<cr>', opts)
                 vim.keymap.set("n", "<leader>lr", '<cmd>LspRestart<cr>', opts)
-                lsp_zero.default_keymaps({
-                    buffer = bufnr,
-                    exclude = {
-                        "<F2>",
-                        "<F4>",
-                    },
-                })
+                vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+                vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+                vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+                vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+                vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+                vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+                vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+                vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+
+                vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
+                vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
+                vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
+
+                -- lsp_zero.default_keymaps({
+                --     buffer = bufnr,
+                --     exclude = {
+                --         "<F2>",
+                --         "<F4>",
+                --     },
+                -- })
             end)
 
             require("mason").setup({})
