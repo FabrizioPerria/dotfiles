b58dee37a1bfbfb7937892c30c040b89bc8cf8d6
Fabrizio Perria <fabrizio.perria@gmail.com>
2024-09-20 00:45:46 +0200

    use picom

--- shell/exports.zsh
+++ shell/exports.zsh
@@ -3,7 +3,7 @@ export FZF_DEFAULT_OPTS="--preview 'bat --color=always {}'"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="fd --type d --hidden --exclude .git --exclude node_modules --exclude .venv"
 
-export PATH="$HOME/.local/bin:/opt/homebrew/opt/gawk/libexec/gnubin:/opt/homebrew/bin:/opt/homebrew/opt/llvm/bin:$PATH:/opt/local/bin"
+export PATH="/opt/homebrew/bin:$HOME/.local/bin:/opt/homebrew/opt/gawk/libexec/gnubin:/opt/homebrew/opt/llvm/bin:$PATH:/opt/local/bin"
 export LDFLAGS="-L/opt/homebrew/opt/llvm/lib"
 export CPPFLAGS="-I/opt/homebrew/opt/llvm/include"
 
