dee2a7ada60e73e8f0d8faf7ed3911d76216c4b5
Fabrizio Perria <fabrizio.perria@gmail.com>
2024-10-05 15:22:29 +0200

    i3-autolayout service setup

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,45 +1,41 @@
 local refresh = vim.api.nvim_create_augroup("refresh", {
-    clear = true
+    clear = true,
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
     pattern = { "*" },
     group = refresh,
-    command = "set formatoptions-=cro"
+    command = "set formatoptions-=cro",
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
     pattern = { "dashboard" },
     group = refresh,
-    
-    command = "lua vim.b.miniindentscope_disable=true"
+
+    command = "lua vim.b.miniindentscope_disable=true",
 })
-
-
 
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     pattern = { "*.c", "*.cpp", "*.h", "*.hpp", "CMakeLists.txt" },
     group = refresh,
     -- command = 'nnoremap <buffer> <leader><leader> :w<CR>:vsplit<bar>term ./doit.sh<CR>'
-    command = "nnoremap <buffer> <leader><leader> :wa<CR>:FloatermNew --autoclose=0 ./build.sh<CR>"
+    command = "nnoremap <buffer> <leader><leader> :wa<CR>:FloatermNew --autoclose=0 ./build.sh<CR>",
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
     callback = function()
         vim.lsp.buf.format()
-    end
+    end,
 })
 
 vim.api.nvim_create_autocmd("TermOpen", {
     callback = function()
         vim.cmd("norm G")
         vim.cmd([[nnoremap <buffer> <Esc> :bd!<CR>]])
-    end
+    end,
 })
 
-vim.api.nvim_create_user_command(
-"EnableGo", 
-function()
+vim.api.nvim_create_user_command("EnableGo", function()
     require("mason-tool-installer").setup({
         ensure_installed = {
             "gopls",
@@ -48,40 +44,16 @@ function()
             "impl",
             "gomodifytags",
             "delve",
-        }
+        },
     })
-end, 
-{}
-)
+end, {})
 
-vim.api.nvim_create_user_command(
-"EnableClangd",
-function()
+vim.api.nvim_create_user_command("EnableClangd", function()
     require("mason-tool-installer").setup({
         ensure_installed = {
             "clangd",
             "clang-format",
             "codelldb",
-        }
+        },
     })
-end, 
-{}
-)
-
-vim.api.nvim_create_user_command(
-"EnablePython",
-function()
-    require("mason-tool-installer").setup({
-        ensure_installed = {
-            -- "mypy",
-            "ruff",
-            "black",
-            "isort",
-            "debugpy",
-            -- "python-lsp-server",
-            "basedpyright",
-        }
-    })
-end,
-{}
-)
+end, {})
--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -16,57 +16,64 @@ return {
     -- {
     --     "williamboman/mason-lspconfig.nvim",
     --     config = function()
-        --         require("mason-lspconfig").setup()
-        --     end,
-        -- },
-        {
-            "WhoIsSethDaniel/mason-tool-installer.nvim",
-            config = function()
-                require("mason-tool-installer").setup({
-                    ensure_installed = {
-                        "cmake-language-server",
-                        
-                        "stylua",
-                        "lua-language-server",
-                        
-                        "json-lsp",
-                        "jsonlint",
-                        "jq",
-                        "yaml-language-server",
-                        "yamllint",
-                        "yamlfmt",
-                        
-                        "commitlint",
-                        "gitlint",
-                        
-                        "marksman",
-                        "markdownlint",
-                        "vale",
-                        "write-good",
-                        "cspell",
-                        "proselint",
-                        
-                        "bash-language-server",
-                        "bash-debug-adapter",
-                        "beautysh",
-                        "shfmt",
-                        "shellcheck",
-                        
-                        "ansible-language-server",
-                        "codespell",
-                        "docker-compose-language-service",
-                        "dockerfile-language-server",
-                        "dot-language-server",
-                        "editorconfig-checker",
-                        
-                        "css-lsp",
-                        "tsserver",
-                        "tailwindcss",
-                        "eslint-lsp",
-                        "prettierd",
-                    },
-                })
-            end,
-        },
-    }
-    
+    --         require("mason-lspconfig").setup()
+    --     end,
+    -- },
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        config = function()
+            require("mason-tool-installer").setup({
+                ensure_installed = {
+                    -- "mypy",
+                    "ruff",
+                    "black",
+                    "isort",
+                    "debugpy",
+                    -- "python-lsp-server",
+                    "basedpyright",
+                    "cmake-language-server",
+
+                    "stylua",
+                    "lua-language-server",
+
+                    "json-lsp",
+                    "jsonlint",
+                    "jq",
+                    "yaml-language-server",
+                    "yamllint",
+                    "yamlfmt",
+
+                    "commitlint",
+                    "gitlint",
+
+                    "marksman",
+                    "markdownlint",
+                    "vale",
+                    "write-good",
+                    "cspell",
+                    "proselint",
+
+                    "bash-language-server",
+                    "bash-debug-adapter",
+                    "beautysh",
+                    "shfmt",
+                    "shellcheck",
+
+                    "ansible-language-server",
+                    "codespell",
+                    "docker-compose-language-service",
+                    "dockerfile-language-server",
+                    "dot-language-server",
+                    "editorconfig-checker",
+
+                    "css-lsp",
+                    "tsserver",
+                    "tailwindcss",
+                    "eslint-lsp",
+                    "prettierd",
+                },
+            })
+        end,
+    },
+}
+
