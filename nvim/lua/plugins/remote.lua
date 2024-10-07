7ce975430722698edd19fdd2077682f12044f24f
Fabrizio <fabrizio@radiobotics.com>
2024-10-07 12:47:53 +0200

    LazyDocker install

--- nvim/lua/plugins/remote.lua
+++ nvim/lua/plugins/remote.lua
@@ -1,13 +1,27 @@
 return {
     {
         "amitds1997/remote-nvim.nvim",
-        version = "*",                  -- Pin to GitHub releases
+        version = "*",                       -- Pin to GitHub releases
         dependencies = {
-            "nvim-lua/plenary.nvim",    -- For standard functions
-            "MunifTanjim/nui.nvim",     -- To build the plugin UI
+            "nvim-lua/plenary.nvim",         -- For standard functions
+            "MunifTanjim/nui.nvim",          -- To build the plugin UI
             "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
         },
         config = true,
-    }
-
+    },
+    {
+        "crnvl96/lazydocker.nvim",
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+        },
+        -- setting the keybinding for LazyGit with 'keys' is recommended in
+        -- order to load the plugin when the command is run for the first time
+        keys = {
+            { "<leader>d", "<cmd>LazyDocker<cr>", desc = "LazyGit" },
+        },
+        event = "VeryLazy",
+        opts = {}, -- automatically calls `require("lazydocker").setup()`
+    },
 }
