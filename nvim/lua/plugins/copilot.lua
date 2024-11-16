c57d32d562159a88bdc30d87b129ba0689c15b66
Fabrizio Perria <fabrizio.perria@gmail.com>
2024-11-16 09:44:34 +0100

    Copilot update

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -3,60 +3,84 @@ return {
         "CopilotC-Nvim/CopilotChat.nvim",
         -- Will be merged to main branch soon
         -- branch = "canary",
-        version = "v2.10.1",
+        version = "v3.0.0",
         opts = {
-            mode = "split",
+            mode = "float",
             show_help = "yes",
             question_header = "## User ",
             answer_header = "## Copilot ",
             error_header = "## Error ",
+            separator = "───", -- Separator to use in chat
             auto_follow_cursor = false, -- Don't follow the cursor after getting response
-            -- show_help = false,              -- Show help in virtual text, set to true if that's 1st time using Copilot Chat
+
+            -- model = "gpt-4o", -- GPT model to use, see ':CopilotChatModels' for available models
+            model = "claude-3.5-sonnet",
+            temperature = 0.1, -- GPT temperature
+
+            show_folds = true, -- Shows folds for sections in chat
+            auto_insert_mode = false, -- Automatically enter insert mode when opening window and on new prompt
+            insert_at_end = false, -- Move cursor to end of buffer when inserting text
+            clear_chat_on_new_prompt = false, -- Clears chat on every new prompt
+            highlight_selection = true, -- Highlight selection in the source buffer when in the chat window
+
+            context = nil, -- Default context to use, 'buffers', 'buffer' or none (can be specified manually in prompt via @).
+            history_path = vim.fn.stdpath("data") .. "/copilotchat_history", -- Default path to stored history
+            callback = nil, -- Callback to use when ask response is received
+
+            window = {
+                layout = "float", -- 'vertical', 'horizontal', 'float', 'replace'
+                width = 0.5, -- fractional width of parent, or absolute width in columns when > 1
+                height = 0.5, -- fractional height of parent, or absolute height in rows when > 1
+                -- Options below only apply to floating windows
+                relative = "editor", -- 'editor', 'win', 'cursor', 'mouse'
+                border = "single", -- 'none', single', 'double', 'rounded', 'solid', 'shadow'
+                row = nil, -- row position of the window, default is centered
+                col = nil, -- column position of the window, default is centered
+                title = "Copilot Chat", -- title of chat window
+                footer = nil, -- footer of chat window
+                zindex = 1, -- determines if window is on top or below other floating windows
+            },
+
+            -- default mappings
             mappings = {
-                -- Use tab for completion
                 complete = {
                     detail = "Use @<Tab> or /<Tab> for options.",
                     insert = "<Tab>",
                 },
-                -- Close the chat
                 close = {
                     normal = "q",
                     insert = "<C-c>",
                 },
-                -- Reset the chat buffer
                 reset = {
                     normal = "<C-x>",
                     insert = "<C-x>",
                 },
-                -- Submit the prompt to Copilot
                 submit_prompt = {
                     normal = "<CR>",
                     insert = "<C-CR>",
                 },
-                -- Accept the diff
                 accept_diff = {
                     normal = "<C-y>",
                     insert = "<C-y>",
                 },
-                -- Yank the diff in the response to register
                 yank_diff = {
-                    normal = "gmy",
+                    normal = "gy",
+                    register = '"',
                 },
-                -- Show the diff
                 show_diff = {
-                    normal = "gmd",
+                    normal = "gd",
                 },
-                -- Show the prompt
                 show_system_prompt = {
-                    normal = "gmp",
+                    normal = "gp",
                 },
-                -- Show the user selection
                 show_user_selection = {
-                    normal = "gms",
+                    normal = "gs",
                 },
             },
         },
+
         build = function()
+            -- vim.cmd("make tiktoken")
             vim.defer_fn(function()
                 vim.cmd("UpdateRemotePlugins")
                 vim.notify("CopilotChat - Updated remote plugins. Please restart Neovim.")
@@ -64,8 +88,10 @@ return {
         end,
         lazy = true,
         keys = {
-            { "<leader>c",  ":CopilotChatToggle<CR>", desc = "CopilotChat Toggle prompt" },
-            { "<leader>cc", ":CopilotChat ",          desc = "CopilotChat Inline" },
+            { "<leader>c", ":CopilotChatToggle<CR>", desc = "CopilotChat Toggle prompt" },
+            { "<leader>cc", ":CopilotChat ", desc = "CopilotChat Inline" },
+            { "<leader>cs", ":CopilotChatSave ", desc = "CopilotChat Save" },
+            { "<leader>cl", ":CopilotChatLoad ", desc = "CopilotChat Load" },
         },
     },
     {
@@ -73,7 +99,7 @@ return {
         lazy = true,
         keys = {
             { "<leader>shutup", ":Copilot disable<CR>", desc = "Copilot Disable" },
-            { "<leader>help",   ":Copilot enable<CR>",  desc = "Copilot Enable" },
+            { "<leader>help", ":Copilot enable<CR>", desc = "Copilot Enable" },
         },
         config = function()
             require("copilot").setup({
