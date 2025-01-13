864295787c10a21e5c4b41fa5de65d3d5d20fa69
Fabrizio Perria <fabrizio@radiobotics.com>
2025-01-13 13:31:51 +0100

    Better comment handling

--- nvim/lua/plugins/comments.lua
+++ nvim/lua/plugins/comments.lua
@@ -1,18 +1,27 @@
 return {
-    "echasnovski/mini.comment",
-    -- lazy = true,
-    -- keys = {
-    --     {"<leader>/" },
-    -- },
-    config = function()
-        require("mini.comment").setup({
-            padding = true,
-            mappings = {
-                comment = "<leader>/",
-                comment_line = "<leader>/",
-                comment_visual = "<leader>/",
-                textobject = "gc"
-            }
-        })
-    end
+    {
+        "JoosepAlviste/nvim-ts-context-commentstring",
+        config = function()
+            require('ts_context_commentstring').setup {}
+            vim.g.skip_ts_context_commentstring_module = true
+        end,
+    },
+    {
+        "echasnovski/mini.comment",
+        -- lazy = true,
+        -- keys = {
+        --     {"<leader>/" },
+        -- },
+        config = function()
+            require("mini.comment").setup({
+                padding = true,
+                mappings = {
+                    comment = "<leader>/",
+                    comment_line = "<leader>/",
+                    comment_visual = "<leader>/",
+                    textobject = "gc",
+                },
+            })
+        end,
+    },
 }
