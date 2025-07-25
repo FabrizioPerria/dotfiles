897150d729f290ecac7063cd6e4aee5d6206c5a4
Fabrizio Perria <fabrizio@radiobotics.com>
2025-07-25 12:05:22 +0200

    find pid listening on port

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -15,3 +15,14 @@ alias rf='f(){ rg --hidden -l "$1" . | fzf | xargs nvim; }; f'
 alias gittree="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'"
 alias xcopy="xclip -selection clipboard"
 alias login-db="az account get-access-token --resource-type oss-rdbms --output tsv --query accessToken"
+
+alias login-db-token='echo "{}" | jq --arg token "$(login-db)" '\''.token=$token'\'' > token.json'
+ 
+pid-port() {
+    local port=${1:-8042}
+    if [[ -z "$port" ]]; then
+        echo "Port number cannot be empty."
+        return 1
+    fi
+    lsof -n -i :$port | grep LISTEN
+}
