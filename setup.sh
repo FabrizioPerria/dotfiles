d3bab16a94ecdfbd2c4793b63428afbb433d6502
Fabrizio Perria <fabrizio.perria@gmail.com>
2025-07-31 19:16:07 +0200

    albert from deb

--- setup.sh
+++ setup.sh
@@ -4,13 +4,22 @@ set -e
 
 SKIP_INSTALL=false
 
+# Use sudo only if not already root
+maybe_sudo() {
+    if [ "$EUID" -ne 0 ]; then
+        sudo "$@"
+    else
+        "$@"
+    fi
+}
+
 # Parse arguments
 for arg in "$@"; do
     case $arg in
-        --skip-install)
-            SKIP_INSTALL=true
-            shift
-            ;;
+    --skip-install)
+        SKIP_INSTALL=true
+        shift
+        ;;
     esac
 done
 
@@ -19,10 +28,9 @@ if [ "$SKIP_INSTALL" = false ]; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
         brew install ansible
     elif command -v apt >/dev/null; then
-        sudo apt update
-        sudo apt install -y ansible
+        maybe_sudo apt update
+        maybe_sudo apt install -y ansible
     fi
 fi
 
 ansible-playbook ansible/playbook.yml --ask-become-pass
-
