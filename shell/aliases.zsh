97ddf6d66b9411586f53bb19a2ef4abc80fe7a4f
Fabrizio Perria <fabrizio.perria@gmail.com>
2026-03-20 20:19:25 +0100

    Restore clipboard

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -14,7 +14,14 @@ alias tmux="tmux -u a || tmux -u "
 alias mon="btm"
 alias rf='f(){ rg --hidden -l "$1" . | fzf | xargs nvim; }; f'
 alias gittree="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'"
-alias xcopy="xclip -selection clipboard"
+# alias xcopy="xclip -selection clipboard"
+
+xcopy() {
+    local data=$(cat)
+    local b64=$(echo -n "$data" | base64 | tr -d '\n')
+    printf '\033]52;c;%s\a' "$b64"
+}
+
 alias login-db="az account get-access-token --resource-type oss-rdbms --output tsv --query accessToken"
 
 alias login-db-token='echo "{}" | jq --arg token "$(login-db)" '\''.token=$token'\'' > token.json'
