9617cfb4da24e4c8e21a1296ba38782675b09a42
Fabrizio Perria <fabrizio.perria@gmail.com>
2026-06-13 19:51:46 +0200

    aliases

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -1,4 +1,3 @@
-alias cd='z'
 alias ls='eza $eza_params'
 alias l='eza --git-ignore $eza_params'
 alias ll='eza --all --header --long $eza_params'
@@ -14,10 +13,7 @@ alias tmux="tmux -u a || tmux -u "
 alias mon="btm"
 alias rf='f(){ rg --hidden -l "$1" . | fzf | xargs nvim; }; f'
 alias gittree="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'"
-# alias xcopy="xclip -selection clipboard"
 alias teamcity="tc"
-
-alias bla="echo bla"
 
 xcopy() {
     local data=$(cat)
